328765b77825655a222160af781eb53493d0c718
Dishant Sakhida <dishant.sakhida@samyak.com>
2023-11-22 18:41:41 +0530

    Some changes related to Education Tab

--- app/src/main/java/com/example/portfolio/EducationFragment.kt
+++ app/src/main/java/com/example/portfolio/EducationFragment.kt
@@ -1,27 +1,55 @@
 package com.example.portfolio
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class EducationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_education, container, false)
+        val view = inflater.inflate(R.layout.fragment_education, container, false)
+
+        val clgParentCard: CardView = view.findViewById(R.id.clgParentCard)
+        val clgParentIcon: ImageView = view.findViewById(R.id.clgParentIcon)
+        val twelveParentCard: CardView = view.findViewById(R.id.twelveParentCard)
+        val twelveParentIcon: ImageView = view.findViewById(R.id.twelveParentIcon)
+        val tenParentCard: CardView = view.findViewById(R.id.tenParentCard)
+        val tenParentIcon: ImageView = view.findViewById(R.id.tenParentIcon)
+
+        //Animation for CardViews
+        val sequence = AnimatorSet()
+        val clgParentCardslide = ObjectAnimator.ofFloat(clgParentCard, "translationX", -1500f, 0f)
+        val twelveParentCardslide = ObjectAnimator.ofFloat(twelveParentCard, "translationX", -1500f, 0f)
+        val tenParentCardslide = ObjectAnimator.ofFloat(tenParentCard, "translationX", -1500f, 0f)
+        sequence.playSequentially(clgParentCardslide, twelveParentCardslide, tenParentCardslide)
+        sequence.start()
+
+
+        clgParentIcon.setOnClickListener{ _->
+            val clgParentCardAnimator = ObjectAnimator.ofFloat(clgParentCard,"alpha",1f,0f)
+            clgParentCardAnimator.addListener(object: AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    clgParentCard.visibility = View.GONE
+                }
+            })
+            clgParentCardAnimator.duration = 1000
+            clgParentCardAnimator.start()
+        }
+
+        return view
     }
 
 }
